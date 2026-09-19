cf09664129b6b340cbeb2256cd490eb8e20a8636
Jawollo07 <jannik@mcjj.de>
2026-09-19 18:36:05 +0200

    Migrate team chat command to RouteTree

--- src/main/java/de/jawollo07/siedler/chat/TeamChatCommand.java
+++ src/main/java/de/jawollo07/siedler/chat/TeamChatCommand.java
@@ -1,38 +1,43 @@
 package de.jawollo07.siedler.chat;
 
-import org.powernukkitx.command.Command;
-import org.powernukkitx.command.CommandSender;
-import org.powernukkitx.Player;
-
 import de.jawollo07.siedler.SiedlerPlugin;
 import de.jawollo07.siedler.core.MessageManager;
+import org.powernukkitx.Player;
+import org.powernukkitx.command.Command;
+import org.powernukkitx.command.CommandResult;
+import org.powernukkitx.command.CommandSender;
+import org.powernukkitx.command.route.RouteTree;
+import org.powernukkitx.command.route.node.RouteNode;
+import org.powernukkitx.command.tree.node.StringNode;
+
+import java.util.Objects;
 
 public class TeamChatCommand extends Command {
     private final TeamChat teamChat;
     private final MessageManager messageManager;
     private final String prefix;
+
     public TeamChatCommand(SiedlerPlugin plugin) {
         super("teamchat", "Sendet eine Nachricht an alle Mitglieder deines Teams", "/teamchat <Nachricht>");
-        this.setPermission("siedler.command.teamchat");
-        this.setPermission("siedler.basic");
+        Objects.requireNonNull(plugin, "plugin");
+        setPermission("siedler.basic");
         this.messageManager = new MessageManager();
-        this.setPermissionMessage(messageManager.getCommandMessage("no-permission"));
+        setPermissionMessage(messageManager.getCommandMessage("no-permission"));
         this.teamChat = new TeamChat(plugin);
         this.prefix = messageManager.getPrefix("chat.team-chat");
+        enableCommandTree();
     }
+
     @Override
-    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        if (args.length < 1) {
-            sender.sendMessage(prefix + messageManager.getMessage("chat", "team-chat.command-usage"));
-            return false;
-        }
-
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage(prefix + messageManager.getCommandMessage("only-player-command"));
-            return false;
-        }
-
-        String message = String.join(" ", args);
-        return teamChat.send(player, message);
+    protected void buildCommandTree(RouteTree tree) {
+        tree.getRoot().then(RouteNode.argument("message", new StringNode()).exec(context -> {
+            CommandSender sender = context.getSender();
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage(prefix + messageManager.getCommandMessage("only-player-command"));
+                return CommandResult.success();
+            }
+            teamChat.send(player, context.getArg("message"));
+            return CommandResult.success();
+        }));
     }
 }
