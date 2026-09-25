8043fd6e781ff5897dfdc16d87ff207cc6a39be6
Jawollo07 <jannik@mcjj.de>
2026-09-25 16:03:50 +0200

    include bonus sources in player team lookup

--- src/main/java/de/jawollo07/siedler/team/TeamManager.java
+++ src/main/java/de/jawollo07/siedler/team/TeamManager.java
@@ -307,7 +307,7 @@ public class TeamManager {
             throw new IllegalArgumentException("Player ID must not be blank");
         }
 
-        String sql = "SELECT t.id, t.name, t.color, t.tax_bonus, t.eliminated, "
+        String sql = "SELECT t.id, t.name, t.color, (t.tax_bonus + COALESCE((SELECT SUM(bs.amount) FROM team_bonus_sources bs WHERE bs.team_id = t.id AND bs.permanent = 1), 0)) AS tax_bonus, t.eliminated, "
                 + "t.elimination_block, t.created_at, "
                 + "COALESCE((SELECT tm.balance FROM team_money tm WHERE tm.team_id = t.id LIMIT 1), 0) AS balance "
                 + "FROM players p JOIN teams t ON t.id = p.team_id WHERE p.id = ?";
